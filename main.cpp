242b6243bd8e4a5a190b12a8430226f049cf4330
joss153 <joss153@naver.com>
2022-03-28 17:25:14 +0900

    edit main.cpp

--- main.cpp
+++ main.cpp
@@ -142,7 +142,16 @@ void bubble_sort_descending(int* array, unsigned int size) {
     {
         //#######################################################
         //Implement here
-        ;
+        for (unsigned int j = 0; j < size - 1 - i; j++)
+		{
+            if (array[j] <= array[j + 1])
+            {
+                     int x;
+                     x = array[j];
+                     array[j] = array[j+1] ;
+                     array[j+1] = x;
+            }
+        }
         //#######################################################
     }
 }
@@ -152,7 +161,20 @@ void insertion_sort_descending(int* array, unsigned int size) {
 	{
 		//#######################################################
         //Implement here
-        ;
+        int temp = array[i];
+		int j = i - 1;
+		while(1)
+		{
+             	if (j <0){
+                break;
+                }
+                if(array[j] >= temp){
+                break;
+                }
+                array[j+1] = array[j];
+                j--;
+		}
+		array[j + 1] = temp;
         //#######################################################
 	}
 }
@@ -162,7 +184,17 @@ void selection_sort_descending(int* array, unsigned int size) {
 	{
 		//#######################################################
         //Implement here
-        ;
+        int max = array[i];
+		int max_idx = i;
+		for (unsigned int j = i + 1; j < size; j++)
+		{
+             	 if (array[j] > max)
+                 {
+                        max = array[j];
+                        max_idx = j;
+                 }
+		}
+		swap(array[i], array[max_idx]);
         //#######################################################
 	}
 }
